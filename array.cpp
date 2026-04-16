957d7ff4166f9192a7bf45666004ba0f00175a80
Nabil <nabil707023@gmail.com>
2026-04-16 13:34:57 +0600

    ddd

--- array.cpp
+++ array.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main() {
   
   cout << "Hello World!" << endl;
+  cout << "Welcome to DSA 2.0" << endl;
+   for (int i = 0; i < 5; i++) {
+    cout << "This is iteration " << i + 1 << endl;
+    }
+        
     return 0;
 }
